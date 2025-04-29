034702e4943e31d930c82de4d496cf81cd1a7c25
yashbhalerao167 <yashbhalerao167@gmail.com>
2025-04-30 01:14:24 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,7 @@
-// vite.config.js
-export default {
-  base: '/coursera-react-project/',
-};
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  base: "/coursera-react-project/",
+  plugins: [react()],
+});
